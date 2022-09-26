96c889466d43f1ff9de859082eb38e9e9f7ebdea
Sora <mievaedu@gmail.com>
2022-09-26 12:29:47 -0300

    Desafio finalizado Primeiro desafio rocketseat finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, unstable_enableLogBox, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,16 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask= {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTasks = tasks.map((el)=>{
+      if (el.id === id){
+        el.done = !el.done
+        return el
+      }
+      return el
+    })
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
+    const updatedtasks = tasks.filter(el => el.id != id)
+    setTasks(updatedtasks)
+    }
 
   return (
     <View style={styles.container}>
